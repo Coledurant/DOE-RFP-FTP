9fbc7917e6ffb2a2c09a7ee0d1031f87df0d4e65
Cole Durant <cole.durant@gmail.com>
2019-06-07 18:25:26 -0400

    Model name & begin download process

--- fob_ftp.py
+++ fob_ftp.py
@@ -7,21 +7,29 @@ from tqdm import tqdm
 import time
 import re
 import os
+from datetime import datetime, timedelta
+from tzlocal import get_localzone
 
-import urllib3
-import datetime as dt
-from ftplib import FTP
-import requests
-from bs4 import BeautifulSoup as bs
-from tqdm import tqdm
-import time
-import re
-import os
+
 
 def download_files():
+    root_ftp_url = 'ftp://ftp.fbo.gov/'
 
+    DAY = timedelta(1)
+    local_tz = get_localzone()
+    now = datetime.now(local_tz)
+    day_ago = local_tz.normalize(now - DAY)
+    naive = now.replace(tzinfo=None) - DAY
+    yesterday = local_tz.localize(naive, is_dst=None).strftime('%Y%m%d')
 
+    model_name = 'FBOFeed' + yesterday
 
+    print(model_name)
 
-
-    return
+    #downloading part
+    http = urllib3.PoolManager()
+    r = http.request('GET', model_name)
+    if r.status != 200:
+        raise ValueError("The url the model files can't be loaded")
+    else:
+        soup = bs(r.data, 'html.parser')
---
+++ run.py
@@ -0,0 +1,11 @@
+import urllib3
+import datetime as dt
+from ftplib import FTP
+import requests
+from bs4 import BeautifulSoup as bs
+from tqdm import tqdm
+import time
+import re
+import os
+from datetime import datetime, timedelta
+from tzlocal import get_localzone
