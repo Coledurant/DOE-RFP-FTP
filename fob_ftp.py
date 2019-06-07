610ed267265b084f51ca3b9e9aa0f899fe108bab
Cole Durant <cole.durant@gmail.com>
2019-06-07 18:02:57 -0400

    imports

--- fob_ftp.py
+++ fob_ftp.py
@@ -1,1 +1,27 @@
-#test
+import urllib3
+import datetime as dt
+from ftplib import FTP
+import requests
+from bs4 import BeautifulSoup as bs
+from tqdm import tqdm
+import time
+import re
+import os
+
+import urllib3
+import datetime as dt
+from ftplib import FTP
+import requests
+from bs4 import BeautifulSoup as bs
+from tqdm import tqdm
+import time
+import re
+import os
+
+def download_files():
+
+
+
+
+
+    return
