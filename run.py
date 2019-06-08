056cca6d86fef743f1a08e8b5b7a091bbe6cf4df
Cole Durant <cole.durant@gmail.com>
2019-06-08 00:05:17 -0400

    run

--- run.py
+++ run.py
@@ -9,3 +9,9 @@ import re
 import os
 from datetime import datetime, timedelta
 from tzlocal import get_localzone
+
+from fob_ftp import download_files
+
+if __name__ == '__main__':
+
+    download_files()
